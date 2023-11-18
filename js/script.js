cf36417124834a19f65dbe8586e72f49bc8e4979
bennydan7 <owusub243@gmail.com>
2023-11-18 07:51:15 +0000

    added functionality to toogle navbar

---
+++ js/script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// Function to toggle menu in small screen devices
+
+const menuBtn = document.getElementById('menu-btn');
+const closeBtn = document.getElementById('menu-close');
+const navbarlinks = document.querySelector('nav .navbar ul');
+
+menuBtn.addEventListener('click', function () {
+  navbarlinks.classList.add('active');
+});
+
+closeBtn.addEventListener('click', function () {
+  navbarlinks.classList.remove('active');
+});
