67a70d1264e0127901c0a0ce9aecb950b9cd4dde
bennydan7 <owusub243@gmail.com>
2023-11-19 15:10:21 +0000

    added functionality for auto-typing

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// Function to toggle menu in small screen devices.
+// Function to toggle menu in small screen devices
 
 const menuBtn = document.getElementById('menu-btn');
 const closeBtn = document.getElementById('menu-close');
@@ -12,4 +12,27 @@ menuBtn.addEventListener('click', function () {
 
 closeBtn.addEventListener('click', function () {
   navbarlinks.classList.remove('active');
+});
+
+// Function for Auto typing text effect
+const initializeTyped = function (elementSelector, text) {
+  return new Typed(elementSelector, {
+    strings: [text],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true,
+  });
+};
+
+// Implementation for Home page
+document.addEventListener('DOMContentLoaded', function () {
+  const typed1 = initializeTyped(
+    '.auto-input1',
+    'Department of Computer Science'
+  );
+});
+
+// Implementation for About page
+document.addEventListener('DOMContentLoaded', function () {
+  const typed2 = initializeTyped('.auto-input2', 'About Us');
 });
