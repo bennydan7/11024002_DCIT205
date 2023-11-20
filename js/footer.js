3dbb87209f41ac67333d48f1542872e96798ad1d
bennydan7 <owusub243@gmail.com>
2023-11-20 18:11:46 +0000

    Added dynamic navbar functionality

---
+++ js/footer.js
@@ -0,0 +1,77 @@
+const footerHTML = ` <footer>
+      <div class="footer-col">
+        <h3>Programs</h3>
+        <li>Undergraduate Courses</li>
+        <li>Graduate Courses(MPhil Computer Science)</li>
+        <li>Graduate Courses(Msc Computer Science)</li>
+        <li>Graduate Courses(PhD)</li>
+        <li>Academic Calendar</li>
+        <li>Level 200 & 300 Timetable</li>
+        <li>Placements For Level 200 Students</li>
+        <li>Student Complaint Form</li>
+      </div>
+      <div class="footer-col">
+        <h3>Enrollment</h3>
+        <li>Undergraduate</li>
+        <li>Graduate</li>
+        <li>International Students</li>
+        <li>Entry Requirements</li>
+        <li>Contact Us</li>
+      </div>
+      <div class="footer-col">
+        <h3>Research</h3>
+        <li>Research at DCS</li>
+        <li>Research Impact</li>
+        <li>Libraries</li>
+      </div>
+      <div class="footer-col">
+        <h3>About The Department</h3>
+        <li>Introducing DCS</li>
+        <li>Head of Departments Office</li>
+        <li>Faculty</li>
+        <li>Events</li>
+      </div>
+
+      <div class="footer-col">
+        <h3>Newsletter</h3>
+        <p>Stay updated with our latest</p>
+        <div class="subscribe">
+          <input type="text" placeholder="Enter Email Address" />
+          <button type="submit">&#8594;</button>
+        </div>
+      </div>
+
+      <div class="footer-col">
+        <h3>Contact Us</h3>
+        <p>Phone & Email</p>
+        <li><i class="fa fa-phone" aria-hidden="true"></i> +233 302 500 171</li>
+        <li><i class="fa fa-phone" aria-hidden="true"></i> +233 559 145 698</li>
+        <li><i class="fa fa-envelope" aria-hidden="true"></i> dcs@ug.edu.gh</li>
+      </div>
+      <div class="footer-col">
+        <h3>Location</h3>
+        <p>University Of Ghana</p>
+        <p>Legon Campus</p>
+        <p>Opposite Mathematics Department</p>
+      </div>
+      <div class="copyright">
+        <p>Copyright ©2023 All rights reserved</p>
+        <div class="social-links">
+          <a
+            href="https://www.instagram.com/explore/locations/468119891/university-of-ghana/"
+          >
+            <i class="fab fa-facebook"></i>
+          </a>
+          <a href="https://twitter.com/univofghana">
+            <i class="fab fa-twitter"></i>
+          </a>
+          <a
+            href="https://www.instagram.com/explore/locations/468119891/university-of-ghana/"
+          >
+            <i class="fab fa-instagram"></i>
+          </a>
+        </div>
+      </div>
+    </footer> `;
+
+document.body.insertAdjacentHTML('beforeend', footerHTML);
