b442b2ac8b0af213a02bdb6a40d01b328369d940
bennydan7 <owusub243@gmail.com>
2023-11-25 06:34:51 +0000

    added functionality to display active navbar link

--- js/navbar.js
+++ js/navbar.js
@@ -8,8 +8,8 @@ const headerHTML = ` <section class="nav1">
             <li><a class="active" href="index.html">Home</a></li>
             <li><a href="about.html">About Us</a></li>
             <li><a href="events.html">Events</a></li>
-            <li><a href="#">Research</a></li>
-            <li><a href="#">Facilities</a></li>
+            <li><a href="research.html">Research</a></li>
+            <li><a href="faculty.html">Faculty</a></li>
           </ul>
           <i id="menu-btn" class="fa fa-bars"></i>
         </div>
@@ -18,3 +18,19 @@ const headerHTML = ` <section class="nav1">
 `;
 
 document.body.insertAdjacentHTML('afterbegin', headerHTML);
+
+document.addEventListener('DOMContentLoaded', function () {
+  const navLinks = document.querySelectorAll('.navbar ul li a');
+  const currentPage = window.location.pathname;
+
+  navLinks.forEach(function (link) {
+    const linkPath = link.getAttribute('href');
+
+    // Check if the link's path is part of the current page's path
+    if (currentPage.includes(linkPath)) {
+      link.classList.add('active');
+    } else {
+      link.classList.remove('active');
+    }
+  });
+});
